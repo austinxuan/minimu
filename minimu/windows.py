bc772bc51d0c3e2d256f2301223ead60726a64c4
nightttt7 <hym601@163.com>
2018-03-08 18:04:47 +0800

    little change

--- minimu/windows.py
+++ minimu/windows.py
@@ -13,7 +13,7 @@ class _mci:
 
     def send(self, command):
         buffer = c_buffer(255)
-        errorcode = self.w32mci(str(command).encode(), buffer, 254, 0)
+        errorcode = self.w32mci(str(command).encode('utf-8'), buffer, 254, 0)
         if errorcode:
             return errorcode, self.get_error(errorcode)
         else:
@@ -28,42 +28,41 @@ class _mci:
     def directsend(self, txt):
         (err, buf) = self.send(txt)
         if err != 0:
-            print('Error {:s} for "{:s}": {:s}'.format(str(err), txt, buf))
+            print('Error {} for "{}": {}'.format(str(err), txt, buf))
         return (err, buf)
 
 # TODO: detect errors in all mci calls
 class AudioClip(object):
     def __init__(self, filename):
-        filename = filename.replace('/', '\\')
         self.filename = filename
-        self._alias = 'mp3_%s' % str(random.random())
+        self._alias = 'mp3_{}'.format(str(random.random()))
 
         self._mci = _mci()
 
-        self._mci.directsend(r'open "{:s}" alias {:s}'.format(filename, self._alias ))
-        self._mci.directsend('set {:s} time format milliseconds'.format(self._alias))
+        self._mci.directsend(r'open "{}" alias {}'.format(filename, self._alias ))
+        self._mci.directsend('set {} time format milliseconds'.format(self._alias))
 
-        err, buf=self._mci.directsend('status %s length' % self._alias)
+        err, buf=self._mci.directsend('status {} length'.format(self._alias))
         self._length_ms = int(buf)
 
     def volume(self, level):
         """Sets the volume between 0 and 100."""
-        self._mci.directsend('setaudio {:s} volume to {:d}'.format(self._alias, level * 10))
+        self._mci.directsend('setaudio {} volume to {}'.format(self._alias, level * 10))
 
     def play(self, start_ms=None, end_ms=None):
         start_ms = 0 if not start_ms else start_ms
         end_ms = self.milliseconds() if not end_ms else end_ms
-        err,buf=self._mci.directsend('play {:s} from {:d} to {:d}'.format(self._alias, start_ms, end_ms))
+        err,buf=self._mci.directsend('play {} from {} to {}'.format(self._alias, start_ms, end_ms))
 
     def isplaying(self):
         return self._mode() == 'playing'
 
     def _mode(self):
-        err, buf = self._mci.directsend('status {:s} mode'.format(self._alias))
+        err, buf = self._mci.directsend('status {} mode'.format(self._alias))
         return buf
 
     def pause(self):
-        self._mci.directsend('pause {:s}'.format(self._alias))
+        self._mci.directsend('pause {}'.format(self._alias))
 
     def unpause(self):
         self._mci.directsend('resume {}'.format(self._alias))
@@ -72,8 +71,8 @@ class AudioClip(object):
         return self._mode() == 'paused'
 
     def stop(self):
-        self._mci.directsend('stop {:s}'.format(self._alias))
-        self._mci.directsend('seek {:s} to start'.format(self._alias))
+        self._mci.directsend('stop {}'.format(self._alias))
+        self._mci.directsend('seek {} to start'.format(self._alias))
 
     def milliseconds(self):
         return self._length_ms
@@ -81,4 +80,4 @@ class AudioClip(object):
     # TODO: this closes the file even if we're still playing.
     # no good.  detect isplaying(), and don't die till then!
     def __del__(self):
-        self._mci.directsend('close {:s}'.format(self._alias))
+        self._mci.directsend('close {}'.format(self._alias))
