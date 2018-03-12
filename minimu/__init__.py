bb605b997b902d2d9e42b83315293b34e83a7d64
nightttt7 <hym601@163.com>
2018-03-12 15:59:49 +0800

    add Songlist class and loadlist function

--- minimu/__init__.py
+++ minimu/__init__.py
@@ -2,12 +2,18 @@
 
 import os
 
-if os.name == 'nt':
-    from .main import Song as _PlatformSpecificSong
-else:
+if os.name != 'nt':
     raise Exception("can't run on this system.")
 
 def load(filename):
-    # 使用minimu.load()来获取一个Song的实例
     from .main import Song
     return Song(filename)
+
+def loadlist(path):
+    from .songlist import Songlist
+    return Songlist(path)
+
+"""
+todo:
+添加: 播放url,生成歌单,播放歌单(包含本地歌曲/url的配置文件)
+"""
